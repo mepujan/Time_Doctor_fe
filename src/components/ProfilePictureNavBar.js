12a3d460e413846670b512773cee8b61d2916dab
Pujan Gautam <mepujan10@gmail.com>
2023-02-16 10:27:23 -0500

    [fixed] URL Scheme not found error

--- src/components/ProfilePictureNavBar.js
+++ src/components/ProfilePictureNavBar.js
@@ -2,12 +2,12 @@ import { Navbar } from "react-bootstrap"
 
 export const ProfilePictureNavBar = () =>{
     const username = localStorage.getItem("username");
-    const profileImage = localStorage.getItem("profile_pic").toString();
-    console.log(profileImage);
+    const profileImage = localStorage.getItem("profile_pic");
+    const ImageURL = "http://"+profileImage
     return (
         <Navbar.Brand href="#home">
        <img
-          src="http://localhost:3500/images/profile_picture/1676553227662_time_doctor.png"
+          src={ImageURL}
           width="60"
           height="60"
           className="rounded-circle align-top"
