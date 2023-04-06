b19471b7757795550e47d87920d432da51cb7989
Pujan Gautam <mepujan10@gmail.com>
2023-04-05 23:14:37 -0400

    [added] export button added

--- src/admin/components/Event.js
+++ src/admin/components/Event.js
@@ -149,6 +149,7 @@ export const Events = () => {
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                             <h1 className="h2">Events</h1>
+                            <button className="btn btn-danger me-5">Export</button>
                         </div>
                         <div className="container">
                             {error && (
