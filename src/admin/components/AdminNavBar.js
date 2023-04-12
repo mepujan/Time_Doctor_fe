ad57b4e78a59f74ca749fe9e95e18d068ff7dd42
Pujan Gautam <mepujan10@gmail.com>
2023-04-11 22:43:00 -0400

    base url updated

--- src/admin/components/AdminNavBar.js
+++ src/admin/components/AdminNavBar.js
@@ -1,23 +1,27 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { LoginBtn } from '../../components/LoginBtn';
+import { Link } from 'react-router-dom';
 
 export const NavBar = ({ loginBtn }) => {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container fluid>
-        <Navbar.Brand href="/admin/dashboard">
-          <img
-            src="/time_doctor_logo.png"
-            width="80"
-            height="80"
-            style={{ marginLeft: 20,objectFit:"cover" }}
-            className="d-inline-block align-top img-fluid"
-            alt="Time Doctor Logo"
-          />{'  '}
-          <br />
-          <b>Time Doctor</b>
-        </Navbar.Brand>
+        <Link to="/admin/dashboard">
+          <Navbar.Brand>
+            <img
+              src="/time_doctor_logo.png"
+              width="80"
+              height="80"
+              style={{ marginLeft: 20, objectFit: "cover" }}
+              className="d-inline-block align-top img-fluid"
+              alt="Time Doctor Logo"
+            />{'  '}
+            <br />
+            <b>Time Doctor</b>
+          </Navbar.Brand>
+        </Link>
+
 
         {loginBtn && <LoginBtn />}
       </Container>
